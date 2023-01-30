ff6d43b5de3eaed586e0db85916eadaa90bdb13c
hasnain2025 <mehadi.2025@gmail.com>
2023-01-31 00:57:21 +0530

    forgat and reset password added

--- src/components/auth/Forget.jsx
+++ src/components/auth/Forget.jsx
@@ -1,17 +1,49 @@
-import AuthLayout from '../layout/AuthLayout';
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { authRequest } from "../../config/axios";
+import AuthLayout from "../layout/AuthLayout";
 
 const Forget = () => {
+  const [email, setEmail] = useState("");
+  const [loader, setLoader] = useState(false);
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoader(true);
+      const res = await authRequest.post("forget-password", { email });
+      if (res?.data?.success) {
+        toast.success(res?.data?.msg);
+      } else toast.error(res?.data?.msg);
+      setLoader(false);
+    } catch (error) {
+      setLoader(false);
+      toast.error(error);
+      console.log("error", error);
+    }
+  };
   return (
     <AuthLayout>
       <div className="container">
         <div className="form-container">
-          <form>
+          <form onSubmit={(e) => handleSubmit(e)}>
             <h2>Forget Password</h2>
             <div className="input-container">
               <i className="fa fa-envelope"></i>
-              <input type="email" placeholder="Enter email address" />
+              <input
+                type="email"
+                placeholder="Enter email address"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <button type="submit">Forget</button>
+            {loader ? (
+              <button type="submit" disabled>
+                Loading...
+              </button>
+            ) : (
+              <button type="submit">Reset</button>
+            )}
           </form>
         </div>
       </div>
--- src/components/auth/Reset.jsx
+++ src/components/auth/Reset.jsx
@@ -1,21 +1,77 @@
-import AuthLayout from '../layout/AuthLayout';
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { authRequest } from "../../config/axios";
+import AuthLayout from "../layout/AuthLayout";
 
 const Reset = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+  const [loader, setLoader] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      if (data?.password !== data?.confirmPassword) {
+        return toast.error("password and confirm password does not matched");
+      }
+      const path = window.location.href;
+      const token = path.split("token=")?.[1];
+      setLoader(true);
+      const res = await authRequest.post("reset-password", {
+        password: data?.password,
+        token,
+      });
+      if (res?.data?.success) {
+        toast.success(res?.data?.msg);
+        navigate("/");
+      } else toast.error(res?.data?.msg);
+      setLoader(false);
+    } catch (error) {
+      setLoader(false);
+      toast.error(error);
+      console.log("error", error);
+    }
+  };
   return (
     <AuthLayout>
       <div className="container">
         <div className="form-container">
-          <form>
+          <form onSubmit={(e) => handleSubmit(e)}>
             <h2>Reset Password</h2>
             <div className="input-container">
               <i className="fa fa-lock"></i>
-              <input type="password" placeholder="Enter password" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                required
+                onChange={(e) => handleChange(e)}
+              />
             </div>
             <div className="input-container">
               <i className="fa fa-lock"></i>
-              <input type="password" placeholder="Enter confirm password" />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Enter confirm password"
+                required
+                onChange={(e) => handleChange(e)}
+              />
             </div>
-            <button type="submit">Reset</button>
+            {loader ? (
+              <button type="submit" disabled>
+                Loading...
+              </button>
+            ) : (
+              <button type="submit">Login</button>
+            )}
           </form>
         </div>
       </div>
